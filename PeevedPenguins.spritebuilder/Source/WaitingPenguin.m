da6422036fb319d4a16762d8fac231eaa957c654
Jiayu Liu <jimexist@gmail.com>
2015-02-04 20:30:39 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/WaitingPenguin.m
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.m
@@ -23,7 +23,7 @@
     // the animation manager of each node is stored in the 'animationManager' property
     CCAnimationManager* animationManager = self.animationManager;
     // timelines can be referenced and run by name
-    [animationManager runAnimationsForSequenceNamed:@"BlinkAndJump"];
+    [animationManager runAnimationsForSequenceNamed:@"JumpAndBlink"];
 }
 
 @end
